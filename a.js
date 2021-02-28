5f6b85890d98bc77b9f8de1d99474095da75ad7d
jamiemonvile <59850787+jamiemonvile@users.noreply.github.com>
2021-02-28 17:48:37 -0500

    Add files via upload

---
+++ a.js
@@ -0,0 +1,47 @@
+// I integrated moment.js to calculate how many years ago it was when I started making unlined notebooks.
+
+for(let i = 0; i<images.length; i++) {
+  const img_deets = images[i];
+  const imgElement = document.createElement("img");
+  imgElement.setAttribute("src", img_deets.pic);
+  imgElement.setAttribute("alt", img_deets.alt);
+
+  img_deets.element = imgElement;
+  thumbnails.append(imgElement);
+
+  imgElement.addEventListener("click", () =>{
+    const featured = document.getElementById('featured');
+    featured.setAttribute("src", img_deets.pic);
+    featured.setAttribute("alt", img_deets.alt);
+
+    const current_description= document.getElementById('current_description');
+    current_description.textContent = img_deets.description;
+  })
+
+  imgElement.addEventListener("mouseover", () =>{
+    imgElement.setAttribute('class', 'color');
+  })
+
+  imgElement.addEventListener("mouseout", () => {
+    imgElement.removeAttribute('class')
+  })
+}
+
+const hideThumbnails = () => {
+  thumbpanel = document.getElementById('thumbnails')
+  thumbpanel.style.display="none";
+}
+
+const hidethumbButton = document.getElementById('hidethumb');
+hidethumbButton.addEventListener('click', hideThumbnails);
+
+const showThumbnails = () => {
+  thumbpanel = document.getElementById('thumbnails')
+  thumbpanel.style.display="block";
+}
+
+const showthumbButton = document.getElementById('showthumb');
+showthumbButton.addEventListener('click', showThumbnails);
+
+
+document.getElementById("date").innerHTML = moment("20130601", "YYYYMMDD").fromNow();
